e10f466e796ac514bfceb16425a3f66044030267
AnveshNalimela <nalimelaanvesh@gmail.com>
2024-10-22 14:53:37 +0530

    Timer using React.js

---
+++ EAD5-Timer/src/App.jsx
@@ -0,0 +1,51 @@
+import { useRef, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const currentTimer = useRef(null);
+
+  function startTimer() {
+    if (!isRunning) {
+      isRunning(true);
+      currentTimer.current = setInterval(() => {
+        setCount((prevCount) => prevCount + 1);
+      }, 1000); 
+    }
+  }
+  function pauseTimer() {
+    if (isRunning) {
+      clearInterval(currentTimer.current);
+      setIsRunning(false);
+    }
+  }
+  function resetTimer() {
+    if (isRunning) {
+      clearInterval(currentTimer.current);
+      setIsRunning(false);
+      setCount(0);
+    }
+  }
+
+  return (
+    <>
+      <h1>Timer</h1>
+      <p>
+        <code>Time count:</code> {count}
+      </p>
+      <div className="card">
+        <button onClick={startTimer} disabled={!isRunning}>
+          Start
+        </button>
+        <button onClick={pauseTimer} disabled={isRunning}>
+          Pause
+        </button>
+        <button onClick={resetTimer}>Reset</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
